22c34facb3e68a7969b96484560fe6e8081dc0a3
Hasan Ali <hasanrazi2003@gmail.com>
2025-02-05 21:26:20 +0300

    Changing the project style to taboog

--- main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    
-    var header = $(document).find('#mainNavBar');
-    console.log(header)
-
-    $(`<i class="icon-Frame-1-2 DynamicUtlIcon"></i>`).appendTo(header)
-
-})
